bd892fc083e65d46660d85554da10d48b1923407
Katie Klochan <katieklochan@Katies-MBP.fios-router.home>
2020-11-17 11:34:43 -0500

    pushing current files

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development', //process.env.NODE_ENV
+  devServer: {
+    publicPath: '/build/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node-modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
